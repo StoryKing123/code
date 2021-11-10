ca074e22f7d1353dcfe6e9b39d0a7bc15bf62450
StoryKing123 <34782804+StoryKing123@users.noreply.github.com>
2021-11-10 13:10:56 +0800

    add code

---
+++ index.js
@@ -0,0 +1,28 @@
+
+function createStore(reducer,initState){
+    let state;
+    let listeners = [];
+
+    function subscribe(fn){
+        listeners.push(fn)
+    }
+    function dispatch(action){
+        state = reducer(state,action)
+        for(let i =0;i<listeners.length;i++){
+            const listener = listeners[i]
+            listener()
+        }        
+    }
+    function getState(){
+
+        return state;
+    }
+
+
+    const store = {
+        subscribe,
+        dispatch,
+        getState
+    }
+    return store;
+}
