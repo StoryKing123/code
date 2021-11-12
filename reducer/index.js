9440a7d77ff29aad05773de60ce50bc29115ea14
steve <steve.xuan@bamboonetworks.com>
2021-11-12 17:59:06 +0800

    feat: add combineReducers

--- reducer/index.js
+++ reducer/index.js
@@ -1,28 +1,38 @@
+export const createStore = (reducer) => {
+  const subscribeCallbackArr = [];
 
+  let state;
+  const dispatch = (action) => {
+    state = reducer(state, action);
+    for (let i = 0; i < subscribeCallbackArr.length; i++) {
+      subscribeCallbackArr[i]();
+    }
+  };
+  const getState = () => {
+    return state;
+  };
+  const subscribe = (fn) => {
+    subscribeCallbackArr.push(fn);
+  };
+  const store = {
+    dispatch,
+    getState,
+    subscribe,
+  };
+  return store;
+};
 
-export const createStore=(reducer)=>{
-    
-    
-    const subscribeCallbackArr = []
+export const combineReducers = (reducerMap) => {
+    const reducerKeys = Object.keys(reducerMap);
 
-    let state;
-    const dispatch = (action)=>{
-        state = reducer(state,action)
-        for (let i = 0; i <subscribeCallbackArr.length; i++) {
-            subscribeCallbackArr[i]()
-        }
-    }
-    const getState = ()=>{
-        return state
-    }
-    const subscribe = (fn)=>{
-        subscribeCallbackArr.push(fn)
-    }
-    const store = {
-        dispatch,
-        getState,
-        subscribe
-    }
-    return store
+    const reducer = (state={},action)=>{
+        const newState = {}
+        const key = reducerKeys[i]
+        const currentReducer = reducerMap[key]
+        const prevState = state[key]
+        newState[key] = currentReducer(prevState,action)
 
-}
+        return newState;
+    }
+    return reducer;
+};
